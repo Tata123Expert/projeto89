8669cf3f69ef461471ae0dc1904cdc57a4dea5ec
Tata123Expert <117285408+Tata123Expert@users.noreply.github.com>
2023-04-21 09:17:35 -0300

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,10 +1,10 @@
-function addUser() /* Adicionar o nome addUser à function. */
-{
-    player1Name = document.getElementById("player1NameInput").value; /* Adicionar o id do jogador 1, ver no index.html */
-    player2Name = document.getElementById("player2NameInput").value; /* Adicionar o id do jogador 2, ver no index.html */
-  
-      localStorage.setItem("player1Name", player1Name); /* Coloque o nome da chave dentro das aspas que é: player1Name */
-      localStorage.setItem("player2Name", player2Name); /* Coloque o nome da chave dentro das aspas que é: player2Name */
-  
-      window.location = "game_page.html";
-  }
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "game_page.html";
+}
